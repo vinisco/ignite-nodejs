48c924b78a3d6289384e6f6fd86d28e90e1b5b19
William almeida <willliam.dev@gmail.com>
2022-05-18 20:22:52 -0300

    category registration

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,11 @@
 import express from "express";
 
+import { categoriesRoutes } from "./routes/categories.routes";
+
 const app = express();
 
 app.use(express.json());
 
-app.get("/", (request, response) => response.json({ message: "hello" }));
+app.use(categoriesRoutes);
 
-app.listen(3333, () =>
-  console.log("server is running on 'http://localhost:3333'")
-);
+app.listen(3333);
