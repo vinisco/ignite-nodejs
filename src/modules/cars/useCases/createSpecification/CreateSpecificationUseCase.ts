c9234f53e78dc3617566dcb7434c27b91969b59b
William Almeida <willliam.dev@gmail.com>
2022-06-30 13:16:04 -0300

    refactored

--- src/modules/cars/useCases/createSpecification/CreateSpecificationUseCase.ts
+++ src/modules/cars/useCases/createSpecification/CreateSpecificationUseCase.ts
@@ -1,29 +1,29 @@
+import { inject, injectable } from "tsyringe";
+
 import { ISpecificationsRepository } from "../../repositories/ISpecificationsRepository";
 
 interface IRequest {
   name: string;
   description: string;
 }
-
+@injectable()
 class CreateSpecificationUseCase {
-  private specificationsRepository: ISpecificationsRepository;
-
-  constructor(specificationsRepository: ISpecificationsRepository) {
-    this.specificationsRepository = specificationsRepository;
+  constructor(
+    @inject("SpecificationRepository")
+    private specificationsRepository: ISpecificationsRepository
+  ) {
+    /* */
   }
 
-  execute({ name, description }: IRequest): void {
+  async execute({ name, description }: IRequest): Promise<void> {
     const specificationAlreadyExists =
-      this.specificationsRepository.findByName(name);
+      await this.specificationsRepository.findByName(name);
 
     if (specificationAlreadyExists) {
       throw new Error("Specification already exists!");
     }
 
-    this.specificationsRepository.create({
-      name,
-      description,
-    });
+    this.specificationsRepository.create({ name, description });
   }
 }
 
