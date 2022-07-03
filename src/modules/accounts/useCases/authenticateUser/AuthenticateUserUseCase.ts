ca5347a465fa2e7373c58c869a8dedb13ea03ba4
William Almeida <willliam.dev@gmail.com>
2022-07-03 15:52:57 -0300

    Create authenticate user use case, using jwt

---
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -0,0 +1,50 @@
+import { compare } from "bcrypt";
+import { sign } from "jsonwebtoken";
+import { inject, injectable } from "tsyringe";
+
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+
+interface IRequest {
+  email: string;
+  password: string;
+}
+
+interface IResponse {
+  user: {
+    name: string;
+    email: string;
+  };
+  token: string;
+}
+
+@injectable()
+class AuthenticateUserUseCase {
+  constructor(
+    @inject("UsersRepository")
+    private usersRepository: IUsersRepository
+  ) {
+    /* */
+  }
+  async execute({ email, password }: IRequest): Promise<IResponse> {
+    const user = await this.usersRepository.findByEmail(email);
+
+    if (!user) {
+      throw new Error("Email or passwor incorret");
+    }
+
+    const passowrdMatch = await compare(password, user.password);
+
+    if (!passowrdMatch) {
+      throw new Error("Email or passwor incorret");
+    }
+
+    const token = sign({}, "75215c28d69829adc061509f586249002ff6a422", {
+      subject: user.id,
+      expiresIn: "1d",
+    });
+
+    return { user, token };
+  }
+}
+
+export { AuthenticateUserUseCase };
