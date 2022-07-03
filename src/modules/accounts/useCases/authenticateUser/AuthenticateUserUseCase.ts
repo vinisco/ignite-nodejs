d2d9357b9c6a0b4cf92e42313cb3d05ebe317f08
William Almeida <willliam.dev@gmail.com>
2022-07-03 20:24:03 -0300

    create a middleware to catch errors

--- src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -2,6 +2,7 @@ import { compare } from "bcrypt";
 import { sign } from "jsonwebtoken";
 import { inject, injectable } from "tsyringe";
 
+import { AppError } from "../../../../errors/AppError";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 
 interface IRequest {
@@ -29,13 +30,13 @@ class AuthenticateUserUseCase {
     const user = await this.usersRepository.findByEmail(email);
 
     if (!user) {
-      throw new Error("Email or passwor incorret");
+      throw new AppError("Email or passwor incorret", 401);
     }
 
     const passowrdMatch = await compare(password, user.password);
 
     if (!passowrdMatch) {
-      throw new Error("Email or passwor incorret");
+      throw new AppError("Email or passwor incorret", 401);
     }
 
     const token = sign({}, "75215c28d69829adc061509f586249002ff6a422", {
--- src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -1,6 +1,7 @@
 import { hash } from "bcrypt";
 import { inject, injectable } from "tsyringe";
 
+import { AppError } from "../../../../errors/AppError";
 import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 
@@ -16,7 +17,7 @@ class CreateUserUseCase {
     const userAlreadyExists = await this.usersRepository.findByEmail(email);
 
     if (userAlreadyExists) {
-      throw new Error("Email is already being used");
+      throw new AppError("Email is already being used", 400);
     }
 
     const passwordHash = await hash(password, 8);
