0b869d33431079d5c21be82a0f6292ca30e88e39
William Almeida <willliam.dev@gmail.com>
2022-08-11 20:43:42 -0300

    create test to list all availables cars

---
+++ src/modules/cars/useCases/listCars/ListCarsUseCase.spec.ts
@@ -0,0 +1,29 @@
+import { CarsRepositoryInMemory } from "@modules/cars/repositories/in-memory/CarsRepositoryInMemory";
+
+import { ListCarsUseCase } from "./ListCarsUseCase";
+
+let listCarsUseCase: ListCarsUseCase;
+let carsRepositoryInMemory: CarsRepositoryInMemory;
+
+describe("List Cars", () => {
+  beforeEach(() => {
+    carsRepositoryInMemory = new CarsRepositoryInMemory();
+    listCarsUseCase = new ListCarsUseCase(carsRepositoryInMemory);
+  });
+
+  it("should be able list all availabe cars", async () => {
+    const car = await carsRepositoryInMemory.create({
+      name: "gol",
+      description: "só gol de placa",
+      daily_rate: 90,
+      license_plate: "WTF-3301",
+      fine_amount: 80,
+      brand: "volkswagen",
+      category_id: "b3b5b870-37e1-4c57-8bb9-729de60ddc50",
+    });
+
+    const cars = await listCarsUseCase.execute();
+
+    expect(cars).toEqual([car]);
+  });
+});
