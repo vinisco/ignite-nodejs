5dce21640552dd8a5b45b00f512a9d65fa737359
William Almeida <willliam.dev@gmail.com>
2022-08-13 19:29:40 -0300

    pass test

--- src/modules/cars/useCases/listCars/ListCarsUseCase.spec.ts
+++ src/modules/cars/useCases/listCars/ListCarsUseCase.spec.ts
@@ -19,10 +19,58 @@ describe("List Cars", () => {
       license_plate: "WTF-3301",
       fine_amount: 80,
       brand: "volkswagen",
-      category_id: "b3b5b870-37e1-4c57-8bb9-729de60ddc50",
+      category_id: "id_test",
     });
 
-    const cars = await listCarsUseCase.execute();
+    const cars = await listCarsUseCase.execute({});
+
+    expect(cars).toEqual([car]);
+  });
+
+  it("should be able list all availabes cars by name", async () => {
+    const car = await carsRepositoryInMemory.create({
+      name: "saveiro",
+      description: "vruum vruuuummmm",
+      daily_rate: 90,
+      license_plate: "WTF-3901",
+      fine_amount: 80,
+      brand: "volkswagen",
+      category_id: "id_test",
+    });
+
+    const cars = await listCarsUseCase.execute({ name: "saveiro" });
+
+    expect(cars).toEqual([car]);
+  });
+
+  it("should be able list all availabes cars by brand", async () => {
+    const car = await carsRepositoryInMemory.create({
+      name: "Uno",
+      description: "2, 3..",
+      daily_rate: 90,
+      license_plate: "WTF-1231",
+      fine_amount: 80,
+      brand: "fiat",
+      category_id: "id_test",
+    });
+
+    const cars = await listCarsUseCase.execute({ brand: "fiat" });
+
+    expect(cars).toEqual([car]);
+  });
+
+  it("should be able list all availabes cars by category_id", async () => {
+    const car = await carsRepositoryInMemory.create({
+      name: "Uno",
+      description: "2, 3..",
+      daily_rate: 90,
+      license_plate: "WTF-1231",
+      fine_amount: 80,
+      brand: "fiat",
+      category_id: "test_by_id",
+    });
+
+    const cars = await listCarsUseCase.execute({ category_id: "test_by_id" });
 
     expect(cars).toEqual([car]);
   });
--- src/modules/cars/useCases/listCars/ListCarsUseCase.ts
+++ src/modules/cars/useCases/listCars/ListCarsUseCase.ts
@@ -1,13 +1,23 @@
 import { Car } from "@modules/cars/infra/typeorm/entities/Car";
 import { CarsRepositoryInMemory } from "@modules/cars/repositories/in-memory/CarsRepositoryInMemory";
 
+interface IRequest {
+  category_id?: string;
+  brand?: string;
+  name?: string;
+}
+
 class ListCarsUseCase {
   constructor(private carsRepository: CarsRepositoryInMemory) {
     /*  */
   }
 
-  async execute(): Promise<Car[]> {
-    const cars = await this.carsRepository.findAvailable();
+  async execute({ category_id, brand, name }: IRequest): Promise<Car[]> {
+    const cars = await this.carsRepository.findAvailable(
+      category_id,
+      brand,
+      name
+    );
 
     return cars;
   }
