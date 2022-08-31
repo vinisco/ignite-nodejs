d46285ebbc5ab1b6d81139886f7d2187b66c6f6b
William Almeida <willliam.dev@gmail.com>
2022-08-31 17:53:49 -0300

    :art: Create intefaces for rental
    Create inteface for `create rental DTO` and an interface for `rental repository in memory`

---
+++ src/modules/rentals/repositories/IRentalsRepository.ts
@@ -0,0 +1,10 @@
+import { ICreateRentalDTO } from "../dtos/ICreateRentalDTO";
+import { Rental } from "../infra/typeorm/entities/Rental";
+
+interface IRentalsRepository {
+  create(data: ICreateRentalDTO);
+  findOpenRentalByCar(car_id: string): Promise<Rental>;
+  findOpenRentalByUser(user_id: string): Promise<Rental>;
+}
+
+export { IRentalsRepository };
