75f85e765531e0e90d1814126dcf4bf46abe2652
William Almeida <willliam.dev@gmail.com>
2022-09-01 16:02:36 -0300

    :art: Create rentals repository
    create rentals repository with typeorm structure

--- src/modules/rentals/repositories/IRentalsRepository.ts
+++ src/modules/rentals/repositories/IRentalsRepository.ts
@@ -2,7 +2,7 @@ import { ICreateRentalDTO } from "../dtos/ICreateRentalDTO";
 import { Rental } from "../infra/typeorm/entities/Rental";
 
 interface IRentalsRepository {
-  create(data: ICreateRentalDTO);
+  create(data: ICreateRentalDTO): Promise<Rental>;
   findOpenRentalByCar(car_id: string): Promise<Rental>;
   findOpenRentalByUser(user_id: string): Promise<Rental>;
 }
